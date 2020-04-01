99cdc4b4a2aff685d4e4494f1d567ee79f2debb2
boganandrey <boganandrey@gmail.com>
2020-04-01 16:50:26 +0400

    Урок 3 -v.5 В админке добавлено выпадающее меню для различных действий

--- resources/views/admin/nav.blade.php
+++ resources/views/admin/nav.blade.php
@@ -4,6 +4,11 @@
 <li class="nav-item">
     <a class="nav-link" href="{{ route('admin.index') }}">Админка</a>
 </li>
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('admin.news.add') }}">Добавить новость</a>
+
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Меню</a>
+    <div class="dropdown-menu">
+        <a class="dropdown-item" href="{{ route('admin.news.add') }}">Добавить новость</a>
+        <a class="dropdown-item" href="#">Другие действия</a>
+    </div>
 </li>
