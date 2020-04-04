fd5fa760ba95ede6f149c44bd9e29bccfeb3c49b
boganandrey <boganandrey@gmail.com>
2020-04-04 17:23:48 +0400

    Урок 4 -v.3

--- resources/views/news/category.blade.php
+++ resources/views/news/category.blade.php
@@ -22,6 +22,8 @@
             <p class="h5">{{ $item['title'] }}</p>
             @if(!$item['isPrivate'])
                 <a href="{{ route('news.view', $item['id']) }}">Подробнее...</a>
+            @else
+                <span style="font-size: small; color: #a9a9a9; font-style: italic;">Новость доступна только для зарегистрированных пользователей</span>
             @endif
             <hr>
         @empty
