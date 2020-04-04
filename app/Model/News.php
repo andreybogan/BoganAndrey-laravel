236e963961d2ebff1fd0c6428a38b6663585d374
boganandrey <boganandrey@gmail.com>
2020-04-04 18:45:09 +0400

    Урок 4 -v.5

--- app/Model/News.php
+++ app/Model/News.php
@@ -66,4 +66,38 @@ class News
 
         return $arr;
     }
+
+    /**
+     * Метод позволяет преобразовать текст в котором перевод строки обозначен \r\n таким образом, чтобы
+     * каждая строка была обернута в заданный тег.
+     * @param string|null $text - Исходный текст
+     * @param string $tag - Тег в который необходимо обернуть текст.
+     * @return string Возвращает текст обернутый в заданный тег.
+     */
+    public static function wrapTextInTag(?string $text, string $tag)
+    {
+        if ($text == null) {
+            return '';
+        }
+
+        // Преобразуем все переводы строки в PHP_EOL.
+        // Обрабатывает сначала \r\n для избежания их повторной замены.
+        $order = array("\r\n", "\n", "\r");
+        $replace = PHP_EOL;
+        $text = str_replace($order, $replace, $text);
+
+        $newText = '';
+
+        // Разбиваем текст по разделителю PHP_EOL.
+        $arr = explode(PHP_EOL, $text);
+
+        // Каждый не пустой элемент массива обертываем в заданный тег предварительно убрав лишние пробелы.
+        foreach ($arr as $value) {
+            if (!empty($value)) {
+                $newText .= "<{$tag}>" . trim($value) . "</{$tag}>";
+            }
+        }
+
+        return $newText;
+    }
 }
