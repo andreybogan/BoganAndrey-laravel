c7636fc245615df7894cc0080a589be8a3c489b4
boganandrey <boganandrey@gmail.com>
2020-04-18 19:06:28 +0400

    Урок 8 -v.4

---
+++ app/Http/Controllers/User/ProfileController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Auth;
+use Hash;
+
+class ProfileController extends Controller
+{
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+
+        $errors = [];
+
+        if ($request->isMethod('post')) {
+            // Проверяем сопадает введенный текущий пароль с сохраненным паролем.
+            if (!Hash::check($request->post('password'), $user->password)) {
+                $errors['password'] = 'Неверно указан текущий пароль';
+            }
+
+            // Выполняем проверку на совпадение нового пароля с его повтором.
+            if ($request->post('new_password') != $request->post('password_confirmation')) {
+                $errors['new_password'] = 'Пароли не совпадают';
+            }
+
+            if (!$errors) {
+                // Обновляем данные
+                $user->fill([
+                    'name' => $request->post('name'),
+                    'email' => $request->post('email'),
+                    'password' => Hash::make($request->post('new_password')),
+                ]);
+
+                $user->save();
+
+                $request->session()->flash('success', 'Данные пользователя успешно изменены!');
+            }
+
+            return redirect()->route('user.updateProfile')->withErrors($errors);
+        }
+
+        return view('user.update-profile', ['user' => $user]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,8 @@ route::group([
 route::group([
     'prefix' => 'admin',
     'namespace' => 'Admin',
-    'as' => 'admin.'
+    'as' => 'admin.',
+    'middleware' => ['auth', 'is_admin']
 ], function () {
     Route::get('/', 'AdminController@index')->name('index');
     Route::get('/download-json-category', 'AdminController@downloadJsonCategory')->name('downloadJsonCategory');
@@ -42,18 +43,15 @@ route::group([
     Route::get('/category/{some}', function () {
         abort(404);
     });
+});
 
-//    Route::group([
-//        'prefix' => 'category',
-//        'as' => 'category.'
-//    ], function () {
-//        Route::get('/', 'CategoryController@index')->name('index');
-//        Route::match(['get', 'post'], '/create', 'CategoryController@create')->name('create');
-//        Route::get('/edit/{category}', 'CategoryController@edit')->name('edit');
-//        Route::post('/update/{category}', 'CategoryController@update')->name('update');
-//        Route::get('/destroy/{category}', 'CategoryController@destroy')->name('destroy');
-//    });
-
+route::group([
+    'prefix' => 'user',
+    'namespace' => 'User',
+    'as' => 'user.',
+    'middleware' => 'auth'
+], function () {
+    Route::match(['get','post'], '/user/update-profile','ProfileController@update')->name('updateProfile');
 });
 
 Auth::routes();
